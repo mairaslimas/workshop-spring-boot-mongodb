3e1bbe25d88341d6082880e71c351006bbeb127a
Maira Lima <mairas.limas@gmail.com>
2023-08-09 18:44:32 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/mairaslimas/workshopmongo/domain/Post.java
+++ src/main/java/com/mairaslimas/workshopmongo/domain/Post.java
@@ -7,6 +7,8 @@ import java.util.Objects;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.mairaslimas.workshopmongo.dto.AuthorDTO;
+
 @Document
 public class Post implements Serializable {
 
@@ -17,7 +19,7 @@ public class Post implements Serializable {
 	private Date date;
 	private String title;
 	private String body;
-	private User author;
+	private AuthorDTO author;
 	
 	
 
@@ -26,7 +28,7 @@ public class Post implements Serializable {
 
 
 
-	public Post(String id, Date date2, String title, String body, User author) {
+	public Post(String id, Date date2, String title, String body, AuthorDTO author) {
 		super();
 		this.id = id;
 		this.date = date2;
@@ -85,13 +87,13 @@ public class Post implements Serializable {
 
 
 
-	public User getAuthor() {
+	public AuthorDTO getAuthor() {
 		return author;
 	}
 
 
 
-	public void setAuthor(User author) {
+	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
 
