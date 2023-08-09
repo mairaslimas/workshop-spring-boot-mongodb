6e02b0f7d1bf8cc83b1b9ed49f6adc6640d867ea
Maira Lima <mairas.limas@gmail.com>
2023-08-09 19:39:08 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/mairaslimas/workshopmongo/domain/Post.java
+++ src/main/java/com/mairaslimas/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.mairaslimas.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.mairaslimas.workshopmongo.dto.AuthorDTO;
+import com.mairaslimas.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -20,6 +23,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	
 
@@ -95,6 +100,18 @@ public class Post implements Serializable {
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+
+
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 
