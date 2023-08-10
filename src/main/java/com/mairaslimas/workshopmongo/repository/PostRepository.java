a806482e850c6464449f3a71d401840bf049399f
Maira Lima <mairas.limas@gmail.com>
2023-08-10 11:23:34 -0300

    Consulta simples com @Query

--- src/main/java/com/mairaslimas/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/mairaslimas/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.mairaslimas.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.mairaslimas.workshopmongo.domain.Post;
@@ -10,4 +11,8 @@ import com.mairaslimas.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 	List<Post> findByTitleContainingIgnoreCase(String Text);
+	
+	/*https://www.mongodb.com/docs/manual/reference/operator/query/regex/*/
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> findByTitle(String text);
 }
